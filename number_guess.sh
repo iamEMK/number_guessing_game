29bba4ee401c33893b8b2cdb047fac25f4de55ff
MOHANKUMAR E <emkmohan007@gmail.com>
2025-01-29 09:55:22 +0000

    feat: add database integration

--- number_guess.sh
+++ number_guess.sh
@@ -13,3 +13,15 @@ read USERNAME
 
 # Check if username exists in database
 USER_INFO=$($PSQL "SELECT games_played, best_game FROM users WHERE username='$USERNAME'")
+if [[ -z $USER_INFO ]]
+then
+  # If user doesn't exist, create new user
+  INSERT_USER=$($PSQL "INSERT INTO users(username, games_played, best_game) VALUES('$USERNAME', 0, 0)")
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+else
+  # If user exists, get their stats
+  echo "$USER_INFO" | while IFS="|" read GAMES_PLAYED BEST_GAME
+  do
+    echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+  done
+fi
